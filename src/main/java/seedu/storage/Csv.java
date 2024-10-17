9c6c6bb3a6f3348fe9ab382024bda945d261dd4a
danusan-s <141602943+danusan-s@users.noreply.github.com>
2024-10-17 11:46:39 +0800

    Replace multi imports with single imports

--- src/main/java/seedu/storage/Csv.java
+++ src/main/java/seedu/storage/Csv.java
@@ -2,8 +2,16 @@ package seedu.storage;
 
 import seedu.model.Inventory;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Csv {
 
